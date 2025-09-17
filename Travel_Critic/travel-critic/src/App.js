528a28d33b1c3fcbd9db8ffc9a73396683e28c51
Swaby404 <swabyoliver@gmail.com>
2025-09-17 09:14:46 -0400

    changes

--- Travel_Critic/travel-critic/src/App.js
+++ Travel_Critic/travel-critic/src/App.js
@@ -1,27 +1,30 @@
-import logo from './logo.svg';
+////this page is the homepage of the website
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-          Sign in!
-
-        </a>
-      </header>
+     <h1><img src="https://tse2.mm.bing.net/th/id/OIP.4ZMqbmWfLpV8V31xizwsUAHaHa?pid=Api&P=0&h=220" alt="Travel Critics" /></h1>
+      <h1>Welcome to the Travel Critics Home</h1>
+      <button><a className="App-link" href="/login">Login</a></button>
+      <button><a className="App-link" href="/signup">Sign Up</a></button>
+        <h2>Your Gateway to Honest Travel Reviews and Discussions</h2>
+       <p>Your one-stop destination for all travel-related discussions and reviews.</p>
+        <p>Explore our curated list of countries below and click on any country to read reviews, share your experiences, and connect with fellow travelers.</p>
+         
+        <p>Happy traveling and happy reviewing!</p>
     </div>
   );
 }
+
+
+
+        
+      
+    
+
+
 
 export default App;
